f8775c39ffeea0659554fd8aded6e6307e8bb43e
Heavy <imfgamedev@gmail.com>
2025-11-29 20:41:55 +0300

    up

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "MainPlayer-Aviator-0.2.93";
+const cacheName = "MainPlayer-Aviator-0.2.94";
 const contentToCache = [
     "Build/OK_build.loader.js",
     "Build/OK_build.framework.js",
